e9505f751486480765749a9cd29ce189e025d00b
MarcusScottS <marcus.sauceda@smoothstack.com>
2023-02-23 16:55:04 -0600

    first commit

---
+++ restaurant-microservice/src/main/java/com/foodie/restaurantmicroservice/RestaurantMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.foodie.restaurantmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class RestaurantMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMicroserviceApplication.class, args);
+	}
+
+}
