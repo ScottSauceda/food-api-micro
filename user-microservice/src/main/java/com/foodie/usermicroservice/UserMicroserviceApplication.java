16dcd05ca30175eb5542b5e34df4f68cebe0252a
ScottSauceda <marcus.sauceda@codeimmersives.com>
2023-02-23 18:53:51 -0600

    modified application-properties for all micro services

--- user-microservice/src/main/java/com/foodie/usermicroservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/com/foodie/usermicroservice/UserMicroserviceApplication.java
@@ -2,12 +2,20 @@ package com.foodie.usermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserMicroserviceApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "homepage of users API";
 	}
 
 }
